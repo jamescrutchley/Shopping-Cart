c3c2c1c25f1e11a97a1744a457201d1574e23302
James Crutchley <jamescrutchley98@gmail.com>
2023-08-25 15:11:50 +1000

    Add global styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Router from './pages/Router.jsx'
+import './styles/globalStyles.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
