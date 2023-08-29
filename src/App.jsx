a7b2824078375187a015c2ae931bc0bb5ec79fe1
James Crutchley <jamescrutchley98@gmail.com>
2023-08-30 09:53:31 +1000

    Change component name

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header"
+import Nav from "./components/Nav"
+import { Outlet } from "react-router-dom";
+import useLocalStorage from 'use-local-storage'
+
+import styles from './styles/App.module.css'
+
+
+function App() {
+
+    const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+    const toggleTheme = () => {
+        console.log('toggle')
+        const newTheme = (theme === 'dark') ? 'light' : 'dark';
+        setTheme(newTheme);
+    }
+
+  return (
+    <div className={styles.app} data-theme={theme}>
+    <Header />
+    <Nav toggleTheme={toggleTheme}/>
+    <Outlet />
+    <Footer />
+    </div>
+  )
+      
+}
+
+export default App
