54950ce0e866ca0599d8c4cbcaf62ae4e9e1452f
James Crutchley <jamescrutchley98@gmail.com>
2023-09-03 13:51:10 +1000

    Tweak layout & styling

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,23 +3,30 @@ import buttonStyles from "../styles/Buttons.module.css";
 import { useEffect } from "react";
 import ProductControls from "./ProductControls";
 
-const Product = ({data = null, isInCart, addToCart }) => {
+const Product = ({ data = null, isInCart, addToCart }) => {
+  const { title, price, image, description } = data || {};
 
-  const { name, price } = data || {}
-
-  useEffect(() => console.log(isInCart))
+  useEffect(() => console.log(isInCart));
 
   return (
-    <article className={`${styles.product} ${isInCart ? styles.isInCart : ''}`}>
+    <article className={`${styles.product} ${isInCart ? styles.isInCart : ""}`}>
       <a href="">
         <div className={styles.productImg}>
-          <img src="" alt="" />
+
+          <div className={styles.imgOverlay}>
+            <img className={styles.image} src={image} alt="" />
+            <div className={styles.descriptionContainer}>
+              <p>{description}</p>
+            </div>
+            <div className={styles.decoration}></div>
+          </div>
+
         </div>
       </a>
 
       <div className={styles.productInfo}>
-        <p>{name ?? '?'}</p>
-        <p>{price ?? '?'}</p>
+        <p>{title ?? "?"}</p>
+        <p>{price ?? "?"}</p>
         {/* later own component */}
         <div className={styles.productControlsContainer}>
           <ProductControls data={data} addToCart={addToCart} />
