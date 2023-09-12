a4c0a90b56c7c788f48c6bfee857a21ef272baa1
James Crutchley <jamescrutchley98@gmail.com>
2023-09-12 11:22:59 +1000

    Fix cart functionality

--- src/components/ProductControls.jsx
+++ src/components/ProductControls.jsx
@@ -2,6 +2,7 @@ import styles from "../styles/Buttons.module.css";
 import { useState, useContext, useEffect } from "react";
 import QuantityControls from "./QuantityControls";
 
+import check from "../assets/icons/check.svg";
 import CartContext from "../context/CartContext";
 
 const ProductControls = ({ data = null, isInCart = null }) => {
@@ -11,34 +12,58 @@ const ProductControls = ({ data = null, isInCart = null }) => {
 
   useEffect(() => {
     if (data) {
-        const foundItem = cartArray.find((item) => item.id === data.id);
-        setInCart(foundItem ? foundItem.quantity : null);        
+      const foundItem = cartArray.find((item) => item.id === data.id);
+      setInCart(foundItem ? foundItem.quantity : null);
     }
   }, [cartArray, data, inCart]);
 
   const onQuantityChange = (value) => {
     setUserQuantitySelection(value);
-  }
+  };
 
   return (
     <div className={styles.productControls}>
-      <QuantityControls key={inCart} inCart={inCart} onChange={onQuantityChange}/>
+      {inCart ? (
+        <p className={styles.inCartNotif}>
+          In cart <img className={styles.icon} src={check} />
+        </p>
+      ) : null}
+      <QuantityControls
+        key={inCart}
+        inCart={inCart}
+        onChange={onQuantityChange}
+      />
       <div className={styles.addToCartContainer}>
-        {!!inCart && <button onClick={() => addToCart(data, userQuantitySelection)} className={styles.addToCart}> Update Cart</button>}
+        {!!inCart && (
+          <button
+            disabled={inCart === userQuantitySelection}
+            onClick={() => addToCart(data, userQuantitySelection)}
+            className={styles.addToCart}
+          >
+            {" "}
+            Update Cart
+          </button>
+        )}
         {!!inCart || (
-          <button onClick={() => addToCart(data, userQuantitySelection)} className={styles.addToCart}>
+          <button
+            onClick={() => addToCart(data, userQuantitySelection)}
+            className={styles.addToCart}
+          >
             Add To Cart
           </button>
         )}
       </div>
-      {/* visual queue for remove button */}
-      <div
-        className={`${styles.removeFromCartButton} ${
-          inCart ? "enabled" : "disabled"
-        }`}
-      >
-        Remove
-      </div>
+      {/* Visual queue for remove button */}
+      {inCart !== null && (
+        <button
+          className={`${styles.removeFromCartButton} ${
+            inCart ? "enabled" : "disabled"
+          }`}
+          onClick={() => addToCart(data, 0)}
+        >
+          Remove All
+        </button>
+      )}
     </div>
   );
 };
--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -6,26 +6,31 @@ function CartProvider({ children }) {
   const [cartArray, setCartArray] = useState([]);
 
   const addToCart = (product, quant = null) => {
-    const index = cartArray.findIndex((item) => item.id === product.id);
-  
-    if (index !== -1) {
-      // If the product already exists, update the quantity
-      const updatedCartArray = [...cartArray];
-      updatedCartArray[index] = {
-        ...updatedCartArray[index],
-        quantity: quant,
-      };
+    if (quant === 0) {
+      const updatedCartArray = cartArray.filter(
+        (item) => item.id !== product.id
+      );
       setCartArray(updatedCartArray);
     } else {
-      // If the product doesn't exist, add it to the cartArray with quantity
-      setCartArray((prevCart) => [
-        ...prevCart,
-        { ...product, quantity: quant }, // Include quantity directly in the object
-      ]);
+      const index = cartArray.findIndex((item) => item.id === product.id);
+
+      if (index !== -1) {
+        // If the product already exists, update the quantity
+        const updatedCartArray = [...cartArray];
+        updatedCartArray[index] = {
+          ...updatedCartArray[index],
+          quantity: quant,
+        };
+        setCartArray(updatedCartArray);
+      } else {
+        // If the product doesn't exist, add it to the cartArray with quantity
+        setCartArray((prevCart) => [
+          ...prevCart,
+          { ...product, quantity: quant }, // Include quantity directly in the object
+        ]);
+      }
     }
   };
-  
-  
 
   return (
     <CartContext.Provider value={{ cartArray, addToCart }}>
