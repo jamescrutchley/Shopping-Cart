32d3db308c119924610d3c580ffc39884855d930
James Crutchley <jamescrutchley98@gmail.com>
2023-09-13 16:14:10 +1000

    Update image panel

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,39 +2,35 @@ import styles from "../styles/Product.module.css";
 import buttonStyles from "../styles/Buttons.module.css";
 import { useEffect, useState } from "react";
 import ProductControls from "./ProductControls";
-import ImagePanel from './ImagePanel'
+import ImagePanel from "./ImagePanel";
 
 const Product = ({ data = null, isInCart, addToCart }) => {
   const { title, price, image, description } = data || {};
-  const [expand, setExpand] = useState(null);
-
+  const [expand, setExpand] = useState(false);
 
   const expandImage = () => {
-    setExpand(expand ? null : true);
-}
+    setExpand(expand ? false : true);
+  };
 
   return (
     <article className={`${styles.product} ${isInCart ? styles.isInCart : ""}`}>
-        {expand && <ImagePanel img={image} clickBack={expandImage}/>}
+      {expand && <ImagePanel img={image} description={description} clickBack={expandImage} />}
       <a onClick={() => expandImage()}>
         <div className={styles.productImg}>
-
           <div className={styles.imgOverlay}>
             <img className={styles.image} src={image} alt="" />
             <div className={styles.decoration}></div>
           </div>
-
         </div>
       </a>
       <div className={styles.productControlsContainer}>
-          <ProductControls data={data} addToCart={addToCart} />
+        <ProductControls data={data} addToCart={addToCart} />
       </div>
       <div className={styles.productInfo}>
         <p>{title ?? "?"}</p>
         <p>${price ?? "?"} AUD</p>
         <p className={styles.productDescription}>{description}</p>
         {/* later own component */}
-        
       </div>
     </article>
   );
